e54b9088afdcf07d01d02b0a52bd4385b80ebf57
SunPipe <g.intonti@studenti.unisa.it>
2024-05-24 10:46:34 +0200

    [ADD] Integrator action to slx files

--- tuning.m
+++ tuning.m
@@ -39,7 +39,7 @@ G_pade = (mu/(1+s*T))*((1-0.5*tau*s)/(1+0.5*tau*s))
 % pade = pade(tau,1)
 % G_pade = G_senza_pade*pade 
 % Same result, using matlab function
-G_with_integration = G_pade/s
+G_with_integration = G_pade/s/9.5493
 % [num,den]=tfdata(G_pade)
 num_without_pole = [0 -0.1697 6.7333]
 den_without_pole = [0.0030 0.1436 1]
@@ -61,18 +61,19 @@ C= [1 0]
 
 %%
 
-A = [ 0 1 0; 0 -9.2074 61.9639; 0 0 -16.211]
-B = [0; 0; 16.211]
-C = [1 0 0]
-Q = eye(4)
-R = 1
-[K,S,e] = lqi(ss(A,B,C,D),Q,R)
-kr = -1/(C*inv(A-B*K(1:3))*B)
+A1 = [ 0 1 0; 0 -9.2074 61.9639; 0 0 -16.211]
+B1 = [0; 0; 16.211]
+C1 = [1 0 0]
+D1 = 0
+Q = [100 0 0 0; 0 0.1 0 0; 0 0 0.1 0; 0 0 0 5]
+R = 0.001
+[K_tilde1,S,e] = lqi(ss(A1,B1,C1,D1),Q,R)
+kr = -1/(C1*inv(A1-B1*K_tilde1(1:3))*B1)
 
 
 %%
-num = [0 0 -0.2075 6.7258]
-den = [0.0034 0.1395 1 0]
+num = [0 0 -0.2075 6.726]
+den = [0.032 1.332 9.549 0]
 [A,B,C,D]=tf2ss(num,den)
 % Dovrebbe funzionare fino a w circa 1/tau, ovvero 19.8410 nel nostro caso
 %https://lpsa.swarthmore.edu/Representations/SysRepTransformations/TF2SS.html#:~:text=Probably%20the%20most%20straightforward%20method,is%20not%20important%20to%20us.
@@ -96,8 +97,8 @@ b0 = 0
 b1 = -0.2075
 b2 = 6.726
 a3 = 0
-a2 = 1/0.003351
-a1 = 0.1395 / 0.003351
+a2 = 1/0.032
+a1 = 1.332 / 0.032
 A = [0 1 0; 0 0 1;-a3 -a2 -a1]
 B = [0; 0; 1]
 C = [b2 b1 b0]
@@ -108,22 +109,32 @@ R = 1
 kr = -1/(C*inv(A-B*K(1:3))*B)
 
 %% approximation padè in ss
-G_senza_delay = G_senza_pade/s
-num = [0 0 6.726]
-den = [0.1086 1 0]
-[A,B,C,D] = tf2ss(num,den)
+G_senza_delay = G_senza_pade/s/9.5493;
+num = [0 0 6.726];
+den = [1.037 9.549 0];
+[A,B,C,D] = tf2ss(num,den);
 %A = [0 1; 0 -9.2081]
 %B = [0; 61.9337]
 %C = [1 0]
 %D = 0
+%%
+
+sys_without_delay = ss(A,B,C,D);
+Q = [0.0003 0 0; 0 0.1  0; 0 0 1];
+R = 0.01;
+[K_tilde,S,e] = lqi(ss(A,B,C,D),Q,R,0);
+kr = -1/(C*inv(A-B*K_tilde(1:2))*B);
+%%
 sys_with_delay = ss(A,B,C,D,'InputDelay',tau)
 sys_pade = pade(sys_with_delay,1,Inf,Inf)
+%%
 A = sys_pade.A
 B = sys_pade.B
 C = sys_pade.C
+
 D = sys_pade.D
-Q = [0.1 0 0 0; 0 100 0 0; 0 0 0.1 0; 0 0 0 5]
-R = 0.001
-[K_tilde,S,e] = lqi(ss(A,B,C,D),Q,R)
+Q = [0.01 0 0 0; 0 10 0 0; 0 0 0.01 0; 0 0 0 5]
+R =  0.87*10
+[K_tilde,S,e] = lqi(ss(A,B,C,D),Q,R,0)
 kr = -1/(C*inv(A-B*K_tilde(1:3))*B)
 
