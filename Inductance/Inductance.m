75f2be29f591ca3b221ffb3f38987991fe4f34fc
DAVIDE RISI <dvdrsi2017@gmail.com>
2024-06-03 15:47:43 +0200

    [ADD] new controller e inductance

--- Inductance/Inductance.m
+++ Inductance/Inductance.m
@@ -1,45 +1,76 @@
 pwmFreq = 2000;
-Ts = 0.005;
-Ts = 0.005;
+Ts = 0.001;
 Tfs = 0.00025;
 alpha = exp(-Tfs/Ts)
 
 %% Getting the L parameter
 % The reference is 0V in [0,3[ seconds, 12V in [3,6[ seconds and 0V in [6,9[
 current = load('current.mat');
+speed = load('speed.mat');
+voltage = load('voltage.mat');
+
 t = current.data.Time;
 current = current.data.Data;
+speed = speed.data.Data;
+speed = speed*2*pi/60;
+voltage = voltage.data.Data(:,:).';
+current_original = current;
 
+%% Compute the L parameter
 
+% Take the values of speed and current in the interval ]3, 3.5]. The tension after 3 is 12V. The resistance is 2 Ohm. The Ke is 1.4543.
+R = 2;
+Ke = 1.4543; % Vs/rad
+
+% apply a filter to the current
+current = smooth(current, 0.03, 'loess');
+
+% apply a filter to the speed
+windowSize = 120;
+b=(1/windowSize)*ones(1,windowSize);
+a = 1;
+speed = filter(b,a,speed);
+
+% plot the current and the filtered current
 figure
-plot(t,current)
+plot(t, current)
+hold on
+plot(t, current_original)
+hold off
 title('Current')
 xlabel('Time (s)')
 ylabel('Current (A)')
-legend('Current')
+legend('Filtered current', 'Original current')
 
-% At 3 second it is (3,0.0695192)
-% The peak is (3.04,1.69866)
 
-value_at_63 = 0.6321 * (1.69866-0.0695192)
+% Define the start and end times of each range
+ranges = [2.9 3.07; 5.9 6.07; 8.9 9.07];
+% Initialize an array to hold the L values for each range
+L_ranges = zeros(size(ranges, 1), 1);
 
-m = (1.69866-0.0695192)/(3.04-3);
-q = 1.69866-m*3.04;
-t = (value_at_63 - q)/m
+% Loop over each range
+for i = 1:size(ranges, 1)
+    L_values = [];
+    % Select the data in the current range
+    t_range = t(t >= ranges(i, 1) & t <= ranges(i, 2));
+    current_range = current(t >= ranges(i, 1) & t <= ranges(i, 2));
+    speed_range = speed(t >= ranges(i, 1) & t <= ranges(i, 2));
+    voltage_range = voltage(t >= ranges(i, 1) & t <= ranges(i, 2));
 
-tau = t - 3
+    % Calculate L for the current range
+    omega = speed_range;
+    dI_dt = gradient(current_range, t_range);
+    
+    % Remove outliers from L values. Outliers are values that are 3 standard
+    for j = 1:length(dI_dt)
+        if dI_dt(j) ~= 0
+        %if abs(dI_dt(j)) > 0.005
+            L = (voltage_range(j) - R * current_range(j) - Ke * omega(j)) / dI_dt(j);
+            L_values = [L_values L];
+        end
+    end
+    L_ranges(i) = mean(L_values);
+    variance = var(L_values)
+end
 
-% Get the sample at time 5 and 5.005
-
-% sample1_ind = find(t == 5);
-% sample2_ind = find(t == 5.005);
-
-% sample1 = current(sample1_ind);
-% sample2 = current(sample2_ind);
-% slope = (sample2 - sample1)/(5.005 - 5);
-% tau = (sample2 - sample1)/slope;
-
-tau = Ts;
-R = 2;
-L = tau*R;
-
+L = mean(L_ranges)
--- torque/torque.m
+++ torque/torque.m
@@ -10,7 +10,7 @@ km = 0.6876;
 kt = 1/km
 k = kt;
 %Tm = 0.065;
-L = 0.0472; %0.0082;
+L = 0.0023; %0.0082;
 R = 2.0;
 
 Im = 0.0059;
@@ -19,9 +19,7 @@ b = 0.016;
 %% Controller - No Windup
 s = tf('s');
 G = 1/(L*s+R);
-C = 1.6667*(s+30)/s; % sisotool
-
-Ts = 0.005;
+C = 0.5625*(s+80)/s
 Cd = c2d(C,Ts,'Tustin')
 
 %% Controller - Windup
@@ -31,12 +29,39 @@ Gamma_vec = [1 0];
 z_Q_Gamma = tf(Q_vec,Gamma_vec,Ts);
 z_antiwindupFunc = tf(Gamma_vec-P_vec,Gamma_vec,Ts);
 
+%% Plot 
 
-%Kp = 1.6667
-%Ki = 30*1.6667;
-%Ti = 1/Ki;
-%antiwindup = 1/(1+Ti*s);
-%antiwindup_d = c2d(antiwindup,Ts,'Tustin');
-%[num,den] = tfdata(antiwindup_d);
-%num = cell2mat(num)
-%den = cell2mat(den)
+y = out.y;
+u = out.u;
+t = out.t;
+ref = out.r;
+ref = ref(:,:).';
+r = [];
+% create a reference signal with the same length as the output. r contains the reference signal and every 5 seconds the reference changes
+for i = 1:length(t)
+    if t(i) < 3
+        r(i) = ref(1);
+    elseif t(i) < 6
+        r(i) = ref(2);
+    else
+        r(i) = ref(3);
+    end
+end
+
+
+figure(1)
+plot(t,y)
+hold on
+plot(t,r)
+hold off
+title('Torque')
+xlabel('Time (s)')
+ylabel('Torque (N*m)')
+legend('Torque', 'Reference')
+
+figure(2)
+plot(t,u)
+title('Voltage')
+xlabel('Time (s)')
+ylabel('Voltage (V)')
+legend('Voltage')
