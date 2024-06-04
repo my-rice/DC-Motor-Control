8f52f878e45bb912d9c4c2f3ee38603e7fc05ecc
DAVIDE RISI <dvdrsi2017@gmail.com>
2024-06-04 12:35:32 +0200

    [FIX] comments and adjustments

--- torque/torque.m
+++ torque/torque.m
@@ -1,56 +1,33 @@
 clear all, clc;
 %% Embedded parameters for DC motor
-%km = 6.88; rpm -> 0.7205. %k = 0.85; motore 25D
-%Tm = 0.065;
-Ts = 0.005;
+
+Ts = 0.005; % sampling time
+
+% first order lag filter parameters
 Tf = Ts/2;
 Tfs = Tf/20; %0.000125;
 alpha = exp(-Tfs/Tf)
 
+% motor parameters. km is taken from model_approximation.m
 km = 0.6876;
 kt = 1/km
 k = kt;
-%Tm = 0.065;
-L = 0.0023; %0.0082;
-R = 2.0;
+L = 0.0023; % taken from the constructor advise
+R = 2.0; % estimated
 
-Im = 0.0059;
-b = 0.016;
+Im = 0.0059; % this is the inertia value of 25d motor taken as example
+b = 0.016; % this is the friction value of 25d motor taken as example
 
 %% Controller - No Windup
 s = tf('s');
 G = 1/(L*s+R);
-C = 0.5625*(s+80)/s
+C = 0.5625*(s+80)/s % result of sisotool session
+% discretization of the controller
 Cd = c2d(C,Ts,'Tustin')
 
-%% Controller - Windup
-
+%% Controller - anti-Windup
+% Obtaining the anti-windup controller
 [Q_vec,P_vec] = tfdata(Cd,'v');
 Gamma_vec = [1 0];
 z_Q_Gamma = tf(Q_vec,Gamma_vec,Ts);
 z_antiwindupFunc = tf(Gamma_vec-P_vec,Gamma_vec,Ts);
-
-%% Plot 
-
-y = out.y;
-u = out.u;
-t = out.t;
-td = out.td;
-r = out.r(:,:).';
-r = r*ones(length(td),1);
-figure(1)
-plot(t,y)
-hold on
-plot(td,r)
-hold off
-title('Torque with disturbance')
-xlabel('Time (s)')
-ylabel('Torque (N*m)')
-legend('Torque', 'Reference')
-
-figure(2)
-plot(td,u)
-title('Input Voltage')
-xlabel('Time (s)')
-ylabel('Voltage (V)')
-legend('Input Voltage')
